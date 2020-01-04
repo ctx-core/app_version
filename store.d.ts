233f33db354c818791ad6e76c3c05042b1062108
Brian Takita <brian.takita@gmail.com>
2020-01-04 17:22:13 -0500

    feat: store: b__ factories
    ∋ "@ctx-core/object": "^6.5.0"

--- store.d.ts
+++ store.d.ts
@@ -1,2 +1,2 @@
-export declare const __version__app: import("svelte/store").Readable<string | number>;
-export declare const __query__version__app: import("svelte/store").Readable<string>;
+export declare const b__version__app: (ctx?: any, opts?: any) => import("svelte/store").Readable<string | number>;
+export declare const b__query__version__app: (ctx?: any, opts?: any) => import("svelte/store").Readable<string>;
--- store.ts
+++ store.ts
@@ -1,14 +1,17 @@
 import { derived } from 'svelte/store'
-import { __CACHE_VERSION, __VERSION } from '@ctx-core/env/store'
-export const __version__app = derived([
-		__CACHE_VERSION,
-		__VERSION,
-	],
-	([CACHE_VERSION, VERSION])=>
-		CACHE_VERSION
-		|| VERSION
-		|| Math.random())
-export const __query__version__app =
-	derived(__version__app,
+import { _b } from '@ctx-core/object'
+import { b__CACHE_VERSION, b__VERSION } from '@ctx-core/env/store'
+export const b__version__app = _b('__version__app', ctx=>
+	derived([
+			b__CACHE_VERSION(ctx),
+			b__VERSION(ctx),
+		],
+		([CACHE_VERSION, VERSION])=>
+			CACHE_VERSION
+			|| VERSION
+			|| Math.random()))
+export const b__query__version__app = _b('__query__version__app', ctx=>
+	derived(
+		b__version__app(ctx),
 		(version__app:string)=>
-			`v=${encodeURIComponent(version__app)}`)
+			`v=${encodeURIComponent(version__app)}`))
