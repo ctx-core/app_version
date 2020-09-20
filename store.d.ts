fde833b9b35de708db267e9e1124c1863e419314
Brian Takita <brian.takita@gmail.com>
2020-09-20 03:59:05 -0700

    update rush shrinkwraps

--- store.d.ts
+++ store.d.ts
@@ -1,4 +1,4 @@
-export declare const b__version__app: (ctx?: object | undefined, opts?: import("@ctx-core/object")._be_opts_type | undefined) => import("svelte/store").Readable<import("@ctx-core/env").$VERSION_type>;
-export declare const __version__app: import("svelte/store").Readable<import("@ctx-core/env").$VERSION_type>;
+export declare const b__version__app: (ctx?: object | undefined, opts?: import("@ctx-core/object")._be_opts_type | undefined) => import("svelte/store").Readable<string | number>;
+export declare const __version__app: import("svelte/store").Readable<string | number>;
 export declare const b__query__version__app: (ctx?: object | undefined, opts?: import("@ctx-core/object")._be_opts_type | undefined) => import("svelte/store").Readable<string>;
 export declare const __query__version__app: import("svelte/store").Readable<string>;
