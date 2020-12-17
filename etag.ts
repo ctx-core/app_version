eee66d8dd972c143b1af359703359a39819152ca
Brian Takita <brian.takita@gmail.com>
2020-12-16 23:23:57 -0700

    ∋ _atob_Uint32Array

--- etag.ts
+++ etag.ts
@@ -2,7 +2,7 @@
  * ctx-core version etag functions
  * @module @ctx-core/version__app/etag
  */
-import { _Uint32Array__atob } from '@ctx-core/atob'
+import { _atob_Uint32Array } from '@ctx-core/atob'
 export const protocol_version = 0
 /**
  * Extracts weak ETag formatted with:
@@ -16,10 +16,10 @@ export const protocol_version = 0
  */
 export function _ctx__etag(etag) {
 	const etag$ = etag.replace('-', '')
-	const Uint32Array__atob = _Uint32Array__atob(etag$)
-	const protocol_version = Uint32Array__atob[0]
-	const CACHE_VERSION = Uint32Array__atob[1]
-	const timestamp = Uint32Array__atob[2]
+	const atob_Uint32Array = _atob_Uint32Array(etag$)
+	const protocol_version = atob_Uint32Array[0]
+	const CACHE_VERSION = atob_Uint32Array[1]
+	const timestamp = atob_Uint32Array[2]
 	const ctx__etag = {
 		protocol_version,
 		CACHE_VERSION,
