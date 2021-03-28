06c4db667cdb2d2e10b687d80e38b0e5453fef07
Brian Takita <brian.takita@gmail.com>
2021-03-28 18:14:12 -0400

    update dependencies

--- src/app_version_b.d.ts
+++ src/app_version_b.d.ts
@@ -1,2 +1,2 @@
-export declare const app_version_b: import("@ctx-core/object").be_type<import("@ctx-core/store").Readable<string | number>, object>;
+export declare const app_version_b: import("@ctx-core/object").Be<import("@ctx-core/store").Readable<string | number>, object>;
 export { app_version_b as b__version__app };
--- src/app_version_query_b.d.ts
+++ src/app_version_query_b.d.ts
@@ -1,2 +1,2 @@
-export declare const app_version_query_b: import("@ctx-core/object").be_type<import("@ctx-core/store").Readable<string>, object>;
+export declare const app_version_query_b: import("@ctx-core/object").Be<import("@ctx-core/store").Readable<string>, object>;
 export { app_version_query_b as b__query__version__app };
